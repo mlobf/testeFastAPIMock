ee1f950ecdaccca4237e3ed5c5263a08c0835761
mlobf <marcoslemeborbafilho@gmail.com>
2022-05-01 00:22:07 -0300

    Dynamic and Static Routing

--- main.py
+++ main.py
@@ -3,29 +3,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# id here is a path parameter
-@app.get("/parameters/{id}")
-def index(id):
-    return {f"Hello to Paramete's World!!{id}"}
+# Static Routes or Static Functions must be placed at first.
+@app.get("/user/admin")
+def profile():
+    return {f"This a simple admin page"}
 
 
-# id must be a integer now
-@app.get("/parameter_typed/{id}")
-def index(id: int):
-    return {f"Hello to Paramete's World!!{id}"}
-
-
-# id must be a string now
-@app.get("/username/{username}")
-def index(username: str):
-    return {f"Hi my name is {username}"}
-
-
-@app.get("/")
-def index():
-    return "Hello World!!"
-
-
-@app.get("/movies")
-def index():
-    return {"movies list": {"Movie One", "Movie two"}}
+# Dynamics Routes or Dynamics Functions must be placed at first.
+@app.get("/user/{username}")
+def profile(username: str):
+    return {f"This is a profile page for {username}"}
