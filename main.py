d89972e19501fe4495fb2f90ebffff0a18da7159
mlobf <marcoslemeborbafilho@gmail.com>
2022-05-01 00:51:33 -0300

    Using Path and Query Parameters simultaneously

--- main.py
+++ main.py
@@ -10,6 +10,28 @@ def profile():
 
 
 # Dynamics Routes or Dynamics Functions must be placed at first.
+# Path Parameters
 @app.get("/user/{username}")
 def profile(username: str):
     return {f"This is a profile page for {username}"}
+
+
+# Query Parameters
+@app.get("/products")
+def products(id: int, price: int):
+    return {f"Products with an id:{id} and price:{price}"}
+
+
+# Default Values for Query Parameters
+@app.get("/cars")
+def products(id: int = 1, price: int = 10):
+    return {f"Cars with an id:{id} and price:{price}"}
+
+
+# Query Parameters and_
+# Default Values for Query Parameters
+@app.get("/profile/{userid}/comments")
+def profile(userid: int = 10, commentid: int = 100):
+    return {
+        f"Profile page for user with user id {userid} and comment with :{commentid}"
+    }
