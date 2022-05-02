9085750945030aad8072f967693fd3f63cefd99d
mlobf <marcoslemeborbafilho@gmail.com>
2022-05-02 00:45:57 -0300

    Creating and Get all or One

--- product/main.py
+++ product/main.py
@@ -1,10 +1,45 @@
 from fastapi import FastAPI
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
 from . import schemas
+from . import models
+from .database import engine, SessionLocal
 
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.get("/products")
+def get_products(db: Session = Depends(get_db)):
+    products = db.query(models.Product).all()
+    return products
+
+
+@app.get("/product/{id}")
+def get_product(id, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == id).first()
+    return product
+
 
 @app.post("/product")
-def add_product(request: schemas.Product):
+def add_product(request: schemas.Product, db: Session = Depends(get_db)):
+    new_product = models.Product(
+        name=request.name,
+        description=request.description,
+        price=request.price,
+    )
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+
     return request
