8139ba04a869d79f19c76810d5265d9894f5ae2b
mlobf <marcoslemeborbafilho@gmail.com>
2022-05-04 13:45:50 -0300

    How to an add router in FastAPI

--- product/main.py
+++ product/main.py
@@ -3,9 +3,10 @@ from fastapi.params import Depends
 from sqlalchemy.orm import Session
 from . import schemas
 from . import models
-from .database import engine, SessionLocal
+from .database import engine, SessionLocal, get_db
 from typing import List
 from passlib.context import CryptContext
+from .routers import product
 
 
 app = FastAPI()
@@ -14,70 +15,9 @@ models.Base.metadata.create_all(engine)
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+app.include_router(product.router)
 
 
-@app.get("/products", response_model=List[schemas.DisplayProduct])
-def get_products(db: Session = Depends(get_db)):
-    products = db.query(models.Product).all()
-    return products
-
-
-# --------------------------------------------------------------------------------
-@app.get("/product/{id}", response_model=schemas.DisplayProduct)
-def get_product(id, response: Response, db: Session = Depends(get_db)):
-    product = db.query(models.Product).filter(models.Product.id == id).first()
-    if not product:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Product not found."
-        )
-    return product
-
-
-# --------------------------------------------------------------------------------
-@app.delete("/product/{id}")
-def delete_product(id, db: Session = Depends(get_db)):
-    product = (
-        db.query(models.Product)
-        .filter(models.Product.id == id)
-        .delete(synchronize_session=False)
-    )
-    db.commit()
-    return {"Data deleted"}
-
-
-@app.put("/product/{id}")
-def update_product(id, request: schemas.Product, db: Session = Depends(get_db)):
-    product = db.query(models.Product).filter(models.Product.id == id)
-    if not product.first():
-        pass
-    product.update(request.dict())
-    db.commit()
-    return {"Product successfully updated"}
-
-
-@app.post("/product", status_code=status.HTTP_201_CREATED)
-def add_product(request: schemas.Product, db: Session = Depends(get_db)):
-    new_product = models.Product(
-        name=request.name,
-        description=request.description,
-        price=request.price,
-        seller_id=1,
-    )
-    db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
-
-    return request
-
-
-# -----------------------------------------------------------------------------
 @app.post(
     "/seller", response_model=schemas.DisplaySeller, status_code=status.HTTP_201_CREATED
 )
---
+++ product/routers/product.py
@@ -0,0 +1,67 @@
+from fastapi import APIRouter, Response, HTTPException, status
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
+from ..database import get_db
+from .. import models
+from typing import List
+from .. import schemas
+
+
+router = APIRouter()
+
+
+@router.get("/products", response_model=List[schemas.DisplayProduct])
+def get_products(db: Session = Depends(get_db)):
+    products = db.query(models.Product).all()
+    return products
+
+
+# --------------------------------------------------------------------------------
+@router.get("/product/{id}", response_model=schemas.DisplayProduct)
+def get_product(id, response: Response, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == id).first()
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Product not found."
+        )
+    return product
+
+
+# --------------------------------------------------------------------------------
+@router.delete("/product/{id}")
+def delete_product(id, db: Session = Depends(get_db)):
+    product = (
+        db.query(models.Product)
+        .filter(models.Product.id == id)
+        .delete(synchronize_session=False)
+    )
+    db.commit()
+    return {"Data deleted"}
+
+
+@router.put("/product/{id}")
+def update_product(id, request: schemas.Product, db: Session = Depends(get_db)):
+    product = db.query(models.Product).filter(models.Product.id == id)
+    if not product.first():
+        pass
+    product.update(request.dict())
+    db.commit()
+    return {"Product successfully updated"}
+
+
+@router.post("/product", status_code=status.HTTP_201_CREATED)
+def add_product(request: schemas.Product, db: Session = Depends(get_db)):
+    new_product = models.Product(
+        name=request.name,
+        description=request.description,
+        price=request.price,
+        seller_id=1,
+    )
+    db.add(new_product)
+    db.commit()
+    db.refresh(new_product)
+
+    return request
+
+
+# -----------------------------------------------------------------------------
